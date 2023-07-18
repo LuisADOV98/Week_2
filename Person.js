b62ed37a533377680bb8221060ca5f60da6586a6
LuisADOV98 <luisadov26@gmail.com>
2023-07-18 16:05:30 +0200

    subida dia 1

---
+++ Person.js
@@ -0,0 +1,56 @@
+
+
+
+ class Person{
+
+        constructor(nombre, apellido, peso, altura){
+            this.nombre = nombre
+            this.apellido = apellido
+            this.edad = 25
+            this.anyoNacimiento = 1998
+            this.peso = peso
+            this.altura = altura
+            this.colorPelo = "castaño"
+            this.hobbies = ["gaming", "buscar a pepe", "ver tv"]
+        }
+        CalIMC(peso, altura){
+            return (this.altura^2) / this.peso 
+        }
+        
+        CalEdad(esteanyo){
+             return esteanyo - this.anyoNacimiento
+        }
+        printAll() {
+        console.log(`
+            - ${this.nombre}
+            - ${this.apellido}
+            - ${this.anyoNacimiento}
+            - ${this.edad}
+            - ${this.peso}
+            - ${this.altura}
+            - ${this.colorPelo}
+                    `)
+        }
+
+        printHobbies(){
+            console.log(`
+           - ${this.hobbies[0]}
+           - ${this.hobbies[1]}
+           - ${this.hobbies[2]}
+                `);
+        }
+        }
+
+module.exports = {Person}
+
+// let persona = new Person("luis", "de Oliveira", 100, 181)
+
+// console.log(persona.CalIMC(100, 181));
+
+// console.log(persona.CalEdad(2023));
+
+// console.log(persona.printAll());
+
+// console.log(persona.printHobbies());
+
+// src=Person
---
+++ personTest.js
@@ -0,0 +1,11 @@
+
+
+let {Person} = require("./Person");
+
+
+
+let persona = new Person("luis", "vieira", 80, 150)
+
+console.log(persona);
+
+console.log(persona.CalIMC(20, 150));
